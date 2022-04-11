901a4a07f9b9c47df3bc26c8e27489b203595e32
martsevoymo <71368007+martsevoymo@users.noreply.github.com>
2022-04-11 22:00:13 +0300

    Update new.sh

--- new.sh
+++ new.sh
@@ -13,6 +13,29 @@ opkg install \
   luci-app-ttyd \
   nano \
   mc \
+  lumimqtt \
+  mpd-full
+  mpg123 \
+  node-zigbee2mqtt \
+  mosquitto \
+  git-http \
+  mpc \
+  janus==1.0.0 \
+  aioblescan==0.2.12 \
+  hatasmota==0.3.1 \
+  wled==0.13.0 
+  
 echo "Packages are installed"
 sleep 2
-block detect >> /etc/config/fstab
+echo "Adding configuration entries to zigbee2mqtt"
+sleep 2
+sed -i 's/port: 8080/port: 8090/' /etc/zigbee2mqtt/configuration.yaml
+sed -i 's/homeassistant: false/homeassistant: true/' /etc/zigbee2mqtt/configuration.yaml
+echo "Configuration entries in zigbee2mqtt have been added"
+sleep 2
+  
+wget https://raw.githubusercontent.com/DivanX10/OpenWRT-and-Home-Assistant/main/scripts/2021/homeassistant_install_2021_11_4.sh -O - | sh
+
+echo "The installation of the basic set is completed, after 10 seconds the gateway will reboot"
+sleep 10
+reboot 
